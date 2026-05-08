4474b31292805975039fc84a2f87eb96c8ad15f2
Riviets <123061245+Riviets@users.noreply.github.com>
2026-05-08 20:33:58 +0300

    add bird rotation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sounddevice as sd
 import numpy as np
 from pygame import *
 from random import randint
+import os
 
 sr = 16000
 block = 256
@@ -18,6 +19,13 @@ init()
 window_size = 1200, 800
 window = display.set_mode(window_size)
 clock = time.Clock()
+
+bird_image = image.load("bird.png").convert_alpha()
+bird_image = transform.scale(bird_image, (100, 100))
+
+bg_image = image.load("bg.png").convert()
+bg_image = transform.scale(bg_image, window_size)
+pipe_img_raw = image.load("pipe.png").convert_alpha()
 
 player_rect = Rect(150, window_size[1]//2-100, 100, 100)
 
@@ -54,13 +62,24 @@ with sd.InputStream(samplerate=sr, channels=1, blocksize=block, callback=audio_c
         y_vel += gravity
         player_rect.y += int(y_vel)
 
-        window.fill('sky blue')
-        draw.rect(window, 'red', player_rect)
+        window.blit(bg_image, (0, 0))
+        
+        # --- Кут нахилу для птаха ---
+        angle = max(-45, min(25, y_vel * -3)) 
+        rotated_bird = transform.rotate(bird_image, angle)
+        bird_render_rect = rotated_bird.get_rect(center=player_rect.center)
+        window.blit(rotated_bird, bird_render_rect)
+        # -------------------------------------------------------------
 
         for pie in pies[:]:
             if not lose:
                 pie.x -= 10
-            draw.rect(window, 'green', pie)
+            
+            pipe_visual = transform.scale(pipe_img_raw, (pie.width, pie.height))
+            if pie.y == 0:
+                pipe_visual = transform.flip(pipe_visual, False, True)
+            window.blit(pipe_visual, pie)
+
             if pie.x <= -100:
                 pies.remove(pie)
                 score += 0.5
@@ -68,7 +87,7 @@ with sd.InputStream(samplerate=sr, channels=1, blocksize=block, callback=audio_c
                 lose = True
 
         if len(pies) < 8:
-            pipes += generate_pipes(150)
+            pies += generate_pipes(150)
 
         score_text = main_font.render(f'{int(score)}', 1, 'black')
         window.blit(score_text, (window_size[0]//2 - score_text.get_rect().w//2, 40))
